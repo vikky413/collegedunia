76076c766bcd213e33c05b131cc7474bbca43759
VIKRAM SINGH <67967129+vikky413@users.noreply.github.com>
2023-04-18 23:39:35 +0530

    Update register.js

--- src/models/register.js
+++ src/models/register.js
@@ -4,21 +4,26 @@ const userSchema = new Schema({
     fname : {
         type:String,
         required:true,
-        unique:true
+        
     },
     lname : {
         type:String,
         required:true,
-        unique:true
+       
     },
     
     email : {
         type:String,
         required:true,
-        unique:true
+      
     },
     
     pnumber : {
+        type:String,
+        required:true
+    },
+
+    feedback : {
         type:String,
         required:true
     },
